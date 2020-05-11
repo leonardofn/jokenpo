dd0c77756fe8b8f8908de20a78b6512e63cf6842
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2020-05-11 15:25:48 -0300

    Adicionando conteudo principal (textos e imagens)

--- lib/game.dart
+++ lib/game.dart
@@ -12,6 +12,41 @@ class _GameState extends State<Game> {
       appBar: AppBar(
         title: Text("JokenPo"),
       ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              "Escolha do App",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Image.asset("assets/images/padrao.png"),
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              "Escolha uma opção abaixo",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Image.asset("assets/images/pedra.png", height: 100),
+              Image.asset("assets/images/papel.png", height: 100),
+              Image.asset("assets/images/tesoura.png", height: 100),
+            ],
+          ),
+        ],),
     );
   }
 }
