e8474e5c2ed8fd1f02cabbb8fefefa6a58894563
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2020-05-11 14:09:47 -0300

    Definindo a estrutura basica do app

---
+++ lib/game.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Game extends StatefulWidget {
+  @override
+  _GameState createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("JokenPo"),
+      ),
+    );
+  }
+}
